90a1287a975f7a21b848e019ebccbf0fcaa1f6ae
finopsguide <142128051+finopsguide@users.noreply.github.com>
2023-08-15 14:08:46 -0700

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+// import { SITE } from './src/config.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			title: 'My Docsss',
+			logo: {
+				src: './src/assets/images/finopsguide-logo-orig-removebg-preview.png',
+				replacesTitle: true,
+				alt: 'Finops Guide',
 			},
+			social: {
+				github: 'https://github.com/finopsguide/finopsguide.com',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/withastro/starlight/edit/main/',
+			},
+			lastUpdated: true,
 			sidebar: [
 				{
 					label: 'Guides',
@@ -26,5 +36,8 @@ export default defineConfig({
 	],
 
 	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+	// image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+	experimental: {
+    assets: true
+  },
 });
--- src/content/docs/guides/example.md
+++ src/content/docs/guides/example.md
@@ -3,8 +3,58 @@ title: Example Guide
 description: A guide in my new Starlight docs site.
 ---
 
+# test heading 1
+
 Guides lead a user through a specific task they want to accomplish, often with a sequence of steps.
 Writing a good guide requires thinking about what your users are trying to do.
+
+## test heading 2
+
+sadkljsalkdajs
+
+![A starry night sky.](../../../assets/images/avatar-smonsen.png)
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
+
+sadkljsalkdajs
+
+aslkdjaslkdj
 
 ## Further reading
 
